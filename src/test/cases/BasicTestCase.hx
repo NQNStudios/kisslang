2070fdf6a7d18134c855ad8c29e07edc871e3cba
Nat Quayle Nelson <natquaylenelson@gmail.com>
2020-11-13 12:28:32 -0700

    BasicTestCase

---
+++ src/test/cases/BasicTestCase.hx
@@ -0,0 +1,23 @@
+package test.cases;
+
+import utest.Test;
+import utest.Assert;
+
+@:build(kiss.Kiss.build("src/test/cases/BasicTestCase.kiss"))
+class BasicTestCase extends Test {
+	function testStaticVar() {
+		Assert.equals("Howdy", BasicTestCase.message);
+	}
+
+	function testHaxeInsertion() {
+		Assert.equals(23, BasicTestCase.mathResult);
+	}
+
+	function testStaticFunction() {
+		Assert.equals(6, BasicTestCase.myFloor(6.5));
+	}
+
+	function testFuncall() {
+		Assert.equals(7, BasicTestCase.funResult);
+	}
+}
