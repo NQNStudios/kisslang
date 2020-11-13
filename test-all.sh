bc37381a4726841d54677ba3449d62eeb5518642
Nat Quayle Nelson <natquaylenelson@gmail.com>
2020-11-13 15:07:37 -0700

    try haxelib install all for travis test dependencies

--- test-all.sh
+++ test-all.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-TEST_FILES=src/build-scripts/test/*.hxml
+TEST_FILES=src/build-scripts/**/test.hxml
 for TEST_FILE in $TEST_FILES
 do
 	haxe $TEST_FILE
--- test.sh
+++ test.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 
-if [ ! -z "$TRAVIS_OS_NAME" ]
-then
-    haxelib install utest
-    haxelib install hxnodejs
-fi
-
 HISS_TARGET=${HISS_TARGET:-$1}
 HISS_TARGET=${HISS_TARGET:-interp}
-haxe src/build-scripts/test/$HISS_TARGET.hxml
+
+if [ ! -z "$TRAVIS_OS_NAME" ]
+then
+    (cd src/build-scripts && haxelib install all)
+    (cd src/build-scripts/$HISS_TARGET && haxelib install all)
+fi
+
+haxe src/build-scripts/$HISS_TARGET/test.hxml
