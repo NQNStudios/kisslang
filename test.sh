0a53dc20de66ce645cf3de4b2d903c392cb272bc
Nat Quayle Nelson <natquaylenelson@gmail.com>
2020-11-12 20:08:53 -0700

    install dependencies

--- test.sh
+++ test.sh
@@ -1,3 +1,11 @@
 #! /bin/bash
+
+if [ ! -z "$TRAVIS_OS_NAME" ]
+then
+    haxelib install utest
+    haxelib install hxnodejs
+fi
+
 HISS_TARGET=${HISS_TARGET:-$1}
+HISS_TARGET=${HISS_TARGET:-interp}
 haxe src/build-scripts/test/$HISS_TARGET.hxml
