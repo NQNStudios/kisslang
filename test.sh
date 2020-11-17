cb6089614a5fb65e8732be478f22fc971435cc01
Nat Quayle Nelson <natquaylenelson@gmail.com>
2020-11-17 15:45:16 -0700

    Fix project testing for Travis

--- test.sh
+++ test.sh
@@ -3,11 +3,16 @@
 KISS_TARGET=${KISS_TARGET:-$1}
 KISS_TARGET=${KISS_TARGET:-interp}
 
-# For CI tests, force install the dependencies
-if [ ! -z "$TRAVIS_OS_NAME" ]
+if [ "$KISS_TARGET" = "projects" ]
 then
-    (cd src/build-scripts && haxelib install all --always)
-    (cd src/build-scripts/$KISS_TARGET && haxelib install all --always)
-fi
+    KISS_HEADLESS="$TRAVIS_OS_NAME" ./test-projects.sh
+else
+    # For CI tests, force install the dependencies
+    if [ ! -z "$TRAVIS_OS_NAME" ]
+    then
+        (cd src/build-scripts && haxelib install all --always)
+        (cd src/build-scripts/$KISS_TARGET && haxelib install all --always)
+    fi
 
-haxe src/build-scripts/common-args.hxml src/build-scripts/common-test-args.hxml src/build-scripts/$KISS_TARGET/test.hxml
+    haxe src/build-scripts/common-args.hxml src/build-scripts/common-test-args.hxml src/build-scripts/$KISS_TARGET/test.hxml
+fi
