fbf8c5d14f51cec79b8b0d4927a75043ab369394
Nat Quayle Nelson <natquaylenelson@gmail.com>
2020-12-03 13:52:30 -0700

    Solve AOC day 3.1

--- projects/advent-of-code/src/year2020/Solutions.hx
+++ projects/advent-of-code/src/year2020/Solutions.hx
@@ -5,6 +5,7 @@ import StringTools;
 import kiss.Prelude;
 import year2020.Util;
 import year2020.Passwords;
+import year2020.Toboggan;
 
 @:build(kiss.Kiss.build("src/year2020/Solutions.kiss"))
 class Solutions {}
---
+++ projects/advent-of-code/src/year2020/Toboggan.hx
@@ -0,0 +1,7 @@
+package year2020;
+
+import kiss.Prelude;
+import year2020.Util;
+
+@:build(kiss.Kiss.build("src/year2020/Toboggan.kiss"))
+class Toboggan {}
