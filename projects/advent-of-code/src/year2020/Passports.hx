2cacce20d84b79e2b3625ef03a08f90d99506b07
Nat Quayle Nelson <natquaylenelson@gmail.com>
2020-12-04 15:28:20 -0700

    Solve AOC day 4

---
+++ projects/advent-of-code/src/year2020/Passports.hx
@@ -0,0 +1,9 @@
+package year2020;
+
+import kiss.Prelude;
+import kiss.Stream;
+
+using StringTools;
+
+@:build(kiss.Kiss.build("src/year2020/Passports.kiss"))
+class Passports {}
--- projects/advent-of-code/src/year2020/Solutions.hx
+++ projects/advent-of-code/src/year2020/Solutions.hx
@@ -3,10 +3,12 @@ package year2020;
 import haxe.ds.Map;
 import StringTools;
 import kiss.Prelude;
+import kiss.Stream;
 import year2020.Util;
 import year2020.SummingTuples;
 import year2020.Passwords;
 import year2020.Toboggan;
+import year2020.Passports;
 
 @:build(kiss.Kiss.build("src/year2020/Solutions.kiss"))
 class Solutions {}
