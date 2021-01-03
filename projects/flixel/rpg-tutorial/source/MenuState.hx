b252ba12ee9e270840a41197a50021ca9ab128a0
Nat Quayle Nelson <natquaylenelson@gmail.com>
2021-01-02 20:22:59 -0700

    RPG tutorial menu & blue box movement

---
+++ projects/flixel/rpg-tutorial/source/MenuState.hx
@@ -0,0 +1,8 @@
+package;
+
+import flixel.FlxState;
+import flixel.ui.FlxButton;
+import flixel.FlxG;
+
+@:build(kiss.Kiss.build("source/MenuState.kiss"))
+class MenuState extends FlxState {}
---
+++ projects/flixel/rpg-tutorial/source/Player.hx
@@ -0,0 +1,11 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.util.FlxColor;
+import flixel.input.keyboard.FlxKey;
+import flixel.math.FlxPoint;
+import kiss.Prelude;
+
+@:build(kiss.Kiss.build("source/Player.kiss"))
+class Player extends FlxSprite {}
