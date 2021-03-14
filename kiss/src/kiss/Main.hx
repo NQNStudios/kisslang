649b5066cb8fffc71ad3b55c33d659335d1ef606
Nat Quayle Nelson <natquaylenelson@gmail.com>
2021-03-14 11:55:02 -0600

    CLI converts stdin -> kiss expressions -> Haxe -> Stdout

--- kiss/src/kiss/Main.hx
+++ kiss/src/kiss/Main.hx
@@ -1,4 +1,29 @@
 package kiss;
 
-@:build(kiss.Kiss.build("src/kiss/Main.kiss"))
-class Main {}
+#if macro
+import haxe.macro.Expr;
+import kiss.Kiss;
+import kiss.Reader;
+import kiss.Stream;
+
+using tink.MacroApi;
+#end
+
+class Main {
+    static macro function macroMain():Expr {
+        var kissInputStream = Stream.fromString(Sys.stdin().readAll().toString());
+        var k = Kiss.defaultKissState();
+        Reader.readAndProcess(kissInputStream, k, (readerExp) -> {
+            Sys.println(Kiss.readerExpToHaxeExpr(readerExp, k).toString());
+        });
+
+        return macro null;
+    }
+
+    // When called from the command-line, `kiss` converts its stdin output to Haxe expressions
+    // TODO write tests for this
+    // TODO use this to implement runAtRuntime() for sys targets by running a haxe subprocess
+    static function main() {
+        macroMain();
+    }
+}
