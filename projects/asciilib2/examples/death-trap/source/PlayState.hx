5e16fd84f459b3862a161d340c2edfb3f8e8b796
Nat Quayle Nelson <natquaylenelson@gmail.com>
2021-04-25 01:39:40 -0600

    [ascii] letters!

--- projects/asciilib2/examples/death-trap/source/PlayState.hx
+++ projects/asciilib2/examples/death-trap/source/PlayState.hx
@@ -3,6 +3,7 @@ package;
 import flixel.FlxState;
 import asciilib.Game;
 import asciilib.backends.flixel.*;
+import flixel.graphics.FlxGraphic;
 
 class PlayState extends FlxState
 {
@@ -11,7 +12,9 @@ class PlayState extends FlxState
 	override public function create()
 	{
 		super.create();
-		game = new Game("Beware Yon Death Trap", 40, 24, 8, 12, new DeathTrapLogic(), new FlxGraphicsBackend(this));
+		game = new Game("Beware Yon Death Trap", 40, 24, 8, 12, new DeathTrapLogic(),
+			new FlxGraphicsBackend(this, FlxGraphic.fromAssetKey("assets/images/size12.png"),
+				"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz,.;:/?!@#$%^&*()-_=+[]{}~ÁÉÍÑÓÚÜáéíñóúü¡¿0123456789\"'<>|"));
 	}
 
 	override public function update(elapsed:Float)
--- projects/asciilib2/src/asciilib/backends/flixel/FlxGraphicsBackend.hx
+++ projects/asciilib2/src/asciilib/backends/flixel/FlxGraphicsBackend.hx
@@ -6,6 +6,11 @@ import flixel.FlxState;
 import flixel.group.FlxGroup;
 import flixel.FlxSprite;
 import flixel.util.FlxColor;
+import flixel.system.FlxAssets;
+import flixel.graphics.frames.FlxBitmapFont;
+import flixel.math.FlxRect;
+import flixel.math.FlxPoint;
+import flixel.text.FlxBitmapText;
 
 @:build(kiss.Kiss.build())
 class FlxGraphicsBackend implements GraphicsBackend {}
