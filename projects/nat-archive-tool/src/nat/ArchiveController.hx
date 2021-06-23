1602b873bc9840edcecb2adaea1851e5eda0a381
Nat Quayle Nelson <natquaylenelson@gmail.com>
2021-06-23 17:21:03 -0600

    WIP CLI

--- projects/nat-archive-tool/src/nat/ArchiveController.hx
+++ projects/nat-archive-tool/src/nat/ArchiveController.hx
@@ -3,21 +3,26 @@ package nat;
 import kiss.Prelude;
 import kiss.List;
 import kiss.Operand;
-
 import haxe.Constraints;
 
-enum CommandArgument {
+enum CommandArgType {
     SelectedEntry;
     SelectedEntries(min:Null<Int>, max:Null<Int>);
-    Text(minLength:Null<Int>, maxLength:Null<Int>);
+    Text(minLength:Null<Int>, maxLength:Null<Float>); // max length is a float so Math.POSITIVE_INFINITY can be used
     Number(min:Null<Float>, max:Null<Float>, inStepsOf:Null<Float>);
-    Entry;
+    OneEntry; // This constructor must be disambiguated from the typedef "Entry"
     Entries(min:Null<Int>, max:Null<Int>);
 }
 
+typedef CommandArg = {
+    name:String,
+    type:CommandArgType
+};
+
 typedef Command = {
-    args:Array<CommandArgument>,
+    args:Array<CommandArg>,
     handler:Function
+    // Command handlers need to return a ChangeSet
 };
 
 typedef ChangeSet = Array<Entry>;
--- projects/nat-archive-tool/src/nat/ArchiveUI.hx
+++ projects/nat-archive-tool/src/nat/ArchiveUI.hx
@@ -6,25 +6,30 @@ interface ArchiveUI {
     /**
      * Prompt the user to enter text
      */
-    function enterText(?minLength:Int, ?maxLength:Int):String;
+    function enterText(prompt:String, resolve:(String) -> Void, ?minLength:Int, ?maxLength:Float):Void;
 
     /**
      * Prompt the user to enter a number
      */
-    function enterNumber(?min:Float, ?max:Float, ?inStepsOf:Float):Float;
+    function enterNumber(prompt:String, resolve:(Float) -> Void, ?min:Float, ?max:Float, ?inStepsOf:Float):Void;
 
     /**
      * Prompt the user to choose a single Entry
      */
-    function chooseEntry(archive:Archive):Entry;
+    function chooseEntry(prompt:String, archive:Archive, resolve:(Entry) -> Void):Void;
 
     /**
      * Prompt the user to choose multiple Entries
      */
-    function chooseEntries(archive:Archive, ?min:Int, ?max:Int):Array<Entry>;
+    function chooseEntries(prompt:String, archive:Archive, resolve:(Array<Entry>) -> Void, ?min:Int, ?max:Float):Void;
 
     /**
      * Update the interface to reflect changes made to Entries through commands
      */
     function handleChanges(changeSet:ChangeSet):Void;
+
+    /**
+     * Tell the user that something is wrong
+     */
+    function reportError(error:String):Void;
 }
