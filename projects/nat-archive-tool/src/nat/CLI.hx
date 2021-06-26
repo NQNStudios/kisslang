c2eb26d42637155d36d7bbc1eff815408a9b18a3
Nat Quayle Nelson <natquaylenelson@gmail.com>
2021-06-26 11:09:52 -0600

    WIP NAT runCommand and defcommand

--- projects/nat-archive-tool/src/nat/CLI.hx
+++ projects/nat-archive-tool/src/nat/CLI.hx
@@ -4,6 +4,7 @@ import kiss.Prelude;
 import kiss.List;
 import kiss.Operand;
 import sys.FileSystem;
+import nat.ArchiveController.CommandArgType;
 
 @:build(kiss.Kiss.build())
 class CLI implements ArchiveUI {}
