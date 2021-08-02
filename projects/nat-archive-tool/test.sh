d54d19a190ae8c222ce5c69baf39c545c652bae0
Nat Quayle Nelson <natquaylenelson@gmail.com>
2021-08-01 20:16:59 -0600

    test nat-archive-tool on many targets

--- projects/nat-archive-tool/test.sh
+++ projects/nat-archive-tool/test.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
 
-haxelib dev nat-archive-tool .
-haxe test.hxml
+# Run these tests on every target that could be used for a NAT front-end
+# (also to test (#extern) on multiple targets)
+haxe test.hxml py.hxml &&
+haxe test.hxml js.hxml &&
+haxe test.hxml cpp.hxml &&
+haxe test.hxml --interp
