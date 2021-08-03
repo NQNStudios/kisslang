e100fec328d1bb7be2aeb0ddd94c7509a7176f9f
Nat Quayle Nelson <natquaylenelson@gmail.com>
2021-08-02 19:49:40 -0600

    [nat] revert accidental test.sh disabling extra plaforms

--- projects/nat-archive-tool/test.sh
+++ projects/nat-archive-tool/test.sh
@@ -2,7 +2,7 @@
 
 # Run these tests on every target that could be used for a NAT front-end
 # (also to test (#extern) on multiple targets)
-# haxe test.hxml py.hxml &&
-# haxe test.hxml js.hxml &&
-# haxe test.hxml cpp.hxml &&
+haxe test.hxml py.hxml &&
+haxe test.hxml js.hxml &&
+haxe test.hxml cpp.hxml &&
 haxe test.hxml --interp
