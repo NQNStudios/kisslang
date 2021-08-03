6f429c4d43f1f0a66830573c8082e7ea68e4561e
Nat Quayle Nelson <natquaylenelson@gmail.com>
2021-08-02 20:28:52 -0600

    [nat] rename archiveController commands to capital names

--- projects/nat-archive-tool/src/test/TestMain.hx
+++ projects/nat-archive-tool/src/test/TestMain.hx
@@ -4,9 +4,11 @@ import kiss.Kiss;
 import kiss.Prelude;
 import nat.BoolExpInterp;
 import nat.Archive;
+import nat.ArchiveController;
 import nat.Lib;
 import nat.components.*;
 import nat.systems.*;
+import uuid.Uuid;
 
 @:build(kiss.Kiss.build())
 class TestMain {}
