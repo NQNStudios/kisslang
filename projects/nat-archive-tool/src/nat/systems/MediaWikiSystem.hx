7c8a80057fe8f2f6c438502c3a1dda3f9f106b63
Nat Quayle Nelson <natquaylenelson@gmail.com>
2021-09-07 19:43:30 -0600

    NAT docs

--- projects/nat-archive-tool/src/nat/systems/MediaWikiSystem.hx
+++ projects/nat-archive-tool/src/nat/systems/MediaWikiSystem.hx
@@ -5,5 +5,8 @@ import kiss.List;
 import nat.System;
 import haxe.Json;
 
+/**
+ * Base System that processes Entries using the HTTP API for any wiki built with MediaWiki
+ */
 @:build(kiss.Kiss.build())
 class MediaWikiSystem extends System {}
--- projects/nat-archive-tool/src/nat/systems/WikipediaImageSystem.hx
+++ projects/nat-archive-tool/src/nat/systems/WikipediaImageSystem.hx
@@ -6,5 +6,8 @@ import kiss.Prelude;
 using haxe.io.Path;
 using StringTools;
 
+/**
+ * System which downloads and attaches images from Wikipedia pages that match Entries' names. 
+ */
 @:build(kiss.Kiss.build())
 class WikipediaImageSystem extends MediaWikiSystem {}
