1e0ba4b84695dc8ad605e6d828dd4e343d35fd7a
Nat Quayle Nelson <natquaylenelson@gmail.com>
2021-09-15 16:31:41 -0600

    Turn nat-flixel-playground PlayState into an ArchiveUI

--- projects/nat-flixel-playground/source/PlayState.hx
+++ projects/nat-flixel-playground/source/PlayState.hx
@@ -1,4 +1,15 @@
 package;
 
+import kiss.Prelude;
+import kiss.List;
+import sys.FileSystem;
+import nat.Entry;
+import nat.BoolExpInterp;
+import nat.Archive;
+import nat.ArchiveUI;
+import nat.ArchiveController;
+
+using StringTools;
+
 @:build(kiss.Kiss.build())
-class PlayState extends FlxState {}
+class PlayState extends FlxState implements ArchiveUI {}
