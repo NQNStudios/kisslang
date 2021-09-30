7cdec423512ae7150d740e1f8cc87d05f4bd5623
Nat Quayle Nelson <natquaylenelson@gmail.com>
2021-09-30 15:04:18 -0600

    Make text sprites for entries that don't get WikipediaImage images

---
+++ projects/nat-flixel-desktop-playground/source/TextSpriteSystem.hx
@@ -0,0 +1,17 @@
+package;
+
+import kiss.Prelude;
+import kiss.List;
+import nat.System;
+import nat.BoolExpInterp;
+import nat.ArchiveController;
+import nat.components.Positions;
+import flash.display.PNGEncoderOptions;
+import flash.utils.ByteArray;
+import flixel.text.FlxText;
+import sys.io.File;
+
+using nat.Lib;
+
+@:build(kiss.Kiss.build())
+class TextSpriteSystem extends System {}
