13389b341c91bc700be996a3d2e0c0daf5f849b0
Nat Quayle Nelson <natquaylenelson@gmail.com>
2021-10-18 15:31:42 -0400

    Adding ktxt2 editor to Kiss-Vscode

---
+++ projects/kiss-vscode/src/KTxt2EditorProvider.hx
@@ -0,0 +1,10 @@
+import kiss.Prelude;
+import kiss.List;
+import vscode.ExtensionContext;
+import vscode.TextDocument;
+import vscode.CancellationToken;
+import vscode.WebviewPanel;
+import js.lib.Promise;
+
+@:build(kiss.Kiss.build())
+class KTxt2EditorProvider {}
