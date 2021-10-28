385a2dcfa19e4122e5531d2fd52189dc050bc5db
Nat Quayle Nelson <natquaylenelson@gmail.com>
2021-10-28 12:24:12 -0400

    Movie.newActor and ActorFlxSprite constructor

--- projects/hollywoo-flixel/src/hollywoo_flixel/ActorFlxSprite.hx
+++ projects/hollywoo-flixel/src/hollywoo_flixel/ActorFlxSprite.hx
@@ -4,5 +4,14 @@ import kiss.Prelude;
 import kiss.List;
 import flixel.FlxSprite;
 
+typedef AnimationArgs = {
+    name:String,
+    frames:Array<Int>,
+    ?frameRate:Float, // default 30
+    ?looped:Bool, // default true
+    ?flipX:Bool, // default false
+    ?flipY:Bool // default false
+};
+
 @:build(kiss.Kiss.build())
 class ActorFlxSprite extends FlxSprite {}
