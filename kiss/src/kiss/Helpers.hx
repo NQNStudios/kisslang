599ec4a94dec15c024d927426ae64038f3d51be9
Nat Quayle Nelson <natquaylenelson@gmail.com>
2021-11-27 21:07:16 -0700

    fix mapForInterp of hscript generation

--- kiss/src/kiss/Helpers.hx
+++ kiss/src/kiss/Helpers.hx
@@ -367,7 +367,7 @@ class Helpers {
                         pos: subExp.pos,
                         expr: ETry(e, catches)
                     };
-                default: subExp;
+                default: mapForInterp(subExp);
             }
         });
     }
