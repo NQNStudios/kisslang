880712f59cf6448f0c890e5ae1b094a11c298bac
Nat Quayle Nelson <natquaylenelson@gmail.com>
2021-12-05 00:30:24 -0700

    solve AOC day 4 pt 1

---
+++ projects/aoc/src/year2021/Day4.hx
@@ -0,0 +1,14 @@
+package year2021;
+
+import haxe.ds.Option;
+
+typedef Board = {
+    uncalled:Array<Array<Option<Int>>>,
+    called:Array<Array<Option<Int>>>
+};
+
+typedef GameState = {
+    numbersToCall:Array<Int>,
+    boards:Array<Board>,
+    boardsByNumber:Map<Int,Array<Board>>
+};
--- projects/aoc/src/year2021/Solutions2021.hx
+++ projects/aoc/src/year2021/Solutions2021.hx
@@ -2,6 +2,10 @@ package year2021;
 
 import kiss.Prelude;
 import haxe.Constraints;
+import haxe.ds.Option;
+#if day4
+import year2021.Day4;
+#end
 
 @:build(kiss.Kiss.build())
 class Solutions2021 {}
