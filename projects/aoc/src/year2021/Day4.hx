e9d43df34c45d5d34340c6a45440b4a27e1cb966
Nat Quayle Nelson <natquaylenelson@gmail.com>
2021-12-06 14:49:26 -0700

    solve AOC day 4 pt 2

--- projects/aoc/src/year2021/Day4.hx
+++ projects/aoc/src/year2021/Day4.hx
@@ -4,11 +4,12 @@ import haxe.ds.Option;
 
 typedef Board = {
     uncalled:Array<Array<Option<Int>>>,
-    called:Array<Array<Option<Int>>>
+    called:Array<Array<Option<Int>>>,
+    won:Bool
 };
 
 typedef GameState = {
     numbersToCall:Array<Int>,
     boards:Array<Board>,
-    boardsByNumber:Map<Int,Array<Board>>
+    boardsByNumber:Map<Int,Array<Board>>,
 };
