f577b9cc9935e1b2795aa37a2b04dab950c93160
Nat Quayle Nelson <natquaylenelson@gmail.com>
2021-12-07 11:55:18 -0700

    Solve AOC day 5 pt 1

---
+++ projects/aoc/src/year2021/Day5.hx
@@ -0,0 +1,10 @@
+package year2021;
+
+typedef Line = {
+    x1:Int,
+    y1:Int,
+    x2:Int,
+    y2:Int
+};
+
+typedef Point = Array<Int>;
--- projects/aoc/src/year2021/Solutions2021.hx
+++ projects/aoc/src/year2021/Solutions2021.hx
@@ -8,6 +8,9 @@ import haxe.Int64Helper;
 #if day4
 import year2021.Day4;
 #end
+#if day5
+import year2021.Day5;
+#end
 
 @:build(kiss.Kiss.build())
 class Solutions2021 {}
