ed32957755c2ffacecf65c4b09afe4e60a43c6a5
Nat Quayle Nelson <natquaylenelson@gmail.com>
2022-01-27 17:24:55 -0700

    WIP DocumentModel

---
+++ projects/handloose/source/DocumentModel.hx
@@ -0,0 +1,17 @@
+package;
+
+import kiss.Prelude;
+import kiss.List;
+import sys.io.File;
+import sys.FileSystem;
+import flixel.math.FlxRandom;
+
+using haxe.io.Path;
+
+typedef ArrowStuff = {
+    text:String,
+    action:Void->Void
+};
+
+@:build(kiss.Kiss.build())
+class DocumentModel {}
