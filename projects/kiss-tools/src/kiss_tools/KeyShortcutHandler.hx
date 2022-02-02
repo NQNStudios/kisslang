afba4ddc265a58db55e792a3f8e4fdfac8805f2a
Nat Quayle Nelson <natquaylenelson@gmail.com>
2022-02-02 14:58:27 -0700

    kiss-tools KeyShortcutHandler based on kiss-vscode shortcut system

---
+++ projects/kiss-tools/src/kiss_tools/KeyShortcutHandler.hx
@@ -0,0 +1,20 @@
+package kiss_tools;
+
+import kiss.Prelude;
+import kiss.Stream;
+import kiss.List;
+
+typedef PrefixMap<T> = Map<String, ShortcutKey<T>>;
+typedef PrefixMapHandler<T> = (Map<String, ShortcutKey<T>>) -> Void;
+typedef ItemHandler<T> = (T) -> Void;
+typedef FinishHandler = () -> Void;
+typedef BadKeyHandler<T> = (String, PrefixMap<T>) -> Void;
+typedef BadShortcutHandler<T> = (String,ShortcutKey<T>) -> Void;
+
+enum ShortcutKey<T> {
+    Final(item:T);
+    Prefix(keys:PrefixMap<T>);
+}
+
+@:build(kiss.Kiss.build())
+class KeyShortcutHandler<T> {}
--- projects/kiss-tools/src/kiss_tools/Main.hx
+++ projects/kiss-tools/src/kiss_tools/Main.hx
@@ -2,6 +2,7 @@ package kiss_tools;
 
 import kiss.Kiss;
 import kiss.Prelude;
+import kiss_tools.KeyShortcutHandler;
 
 @:build(kiss.Kiss.build())
 class Main {}
