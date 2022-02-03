c8e043b7268a496d09c08be9d6ab3aa2d4983a98
Nat Quayle Nelson <natquaylenelson@gmail.com>
2022-02-03 14:51:53 -0700

    Parse habits.txt model for habit game

--- projects/habit-puzzle-game/source/HabitModel.hx
+++ projects/habit-puzzle-game/source/HabitModel.hx
@@ -2,6 +2,24 @@ package;
 
 import kiss.Prelude;
 import kiss.List;
+import kiss.Stream;
+import sys.io.File;
+
+enum EntryType {
+    Daily(daysOfWeek:Array<Int>);
+    Bonus;
+    Todo;
+}
+
+typedef EntryLabel = {
+    label:String,
+    points:Int
+};
+
+typedef Entry = {
+    type: EntryType,
+    labels: Array<EntryLabel>
+};
 
 @:build(kiss.Kiss.build())
 class HabitModel {}
