5531bc9f5babd14e1abaa9e09ddd47272d69fbb8
Nat Quayle Nelson <natquaylenelson@gmail.com>
2022-02-03 22:41:05 -0700

    habit game make shortcut map and text labels

--- projects/habit-puzzle-game/source/HabitState.hx
+++ projects/habit-puzzle-game/source/HabitState.hx
@@ -2,11 +2,14 @@ package;
 
 import flixel.FlxG;
 import flixel.FlxState;
+import flixel.group.FlxGroup;
 import flixel.FlxSprite;
 import flixel.util.FlxColor;
 import flixel.text.FlxText;
 import kiss.Prelude;
 import kiss.List;
+import kiss_tools.KeyShortcutHandler;
+import HabitModel;
 
 @:build(kiss.Kiss.build())
 class HabitState extends FlxState {}
