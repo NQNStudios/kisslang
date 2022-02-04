d268b0cdb664ee83348cf071490b5d9d57480dfb
Nat Quayle Nelson <natquaylenelson@gmail.com>
2022-02-04 16:53:40 -0700

    Reveal a puzzle in Habit game

--- projects/habit-puzzle-game/source/HabitState.hx
+++ projects/habit-puzzle-game/source/HabitState.hx
@@ -1,16 +1,21 @@
 package;
 
+import flash.display.BitmapData;
+import haxe.io.Path;
 import flixel.FlxG;
 import flixel.FlxState;
 import flixel.group.FlxGroup;
 import flixel.FlxSprite;
 import flixel.util.FlxColor;
 import flixel.text.FlxText;
+import flixel.math.FlxRandom;
+import flixel.math.FlxPoint;
 import kiss.Prelude;
 import kiss.List;
 import kiss_tools.KeyShortcutHandler;
 import HabitModel;
 import flixel.input.keyboard.FlxKey;
+import hx.strings.Strings;
 
 @:build(kiss.Kiss.build())
 class HabitState extends FlxState {}
