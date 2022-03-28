03f0e65a2b4d9b0d36b1e5b55b396935b39dc501
Nat Quayle Nelson <natquaylenelson@gmail.com>
2022-03-28 15:13:25 -0600

    fix kiss-vscode tests

--- projects/kiss-vscode/test.sh
+++ projects/kiss-vscode/test.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 # Run the build without -D test first, to make sure it works that way too:
-haxe build.hxml && haxe -D test build.hxml -cmd "node bin/extension.js"
+echo "!test" && haxe build.hxml && echo "test" && haxe -D test build.hxml -cmd "node bin/extension.js"
