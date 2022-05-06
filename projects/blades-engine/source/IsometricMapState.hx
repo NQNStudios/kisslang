3cfb9fdc862b792b751590fb6c10b1aab74ba583
Nat Quayle Nelson <natquaylenelson@gmail.com>
2022-05-06 16:44:08 -0400

    blades-engine render one tile

---
+++ projects/blades-engine/source/IsometricMapState.hx
@@ -0,0 +1,16 @@
+package;
+
+import flixel.FlxState;
+import flixel.FlxSprite;
+import flixel.util.FlxColor;
+import flixel.math.FlxRect;
+import flixel.math.FlxVector;
+import flixel.graphics.FlxGraphic;
+import flixel.FlxG;
+
+import flash.display.BitmapData;
+
+import kiss.Prelude;
+
+@:build(kiss.Kiss.build())
+class IsometricMapState extends FlxState {}
