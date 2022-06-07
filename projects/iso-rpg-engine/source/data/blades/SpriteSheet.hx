58b1211fadb8410af45ef547d34109e17539087e
Nat Quayle Nelson <natquaylenelson@gmail.com>
2022-06-07 18:53:44 +0000

    iso engine handle blades spritesheet properly

---
+++ projects/iso-rpg-engine/source/data/blades/SpriteSheet.hx
@@ -0,0 +1,13 @@
+package data.blades;
+
+import flixel.FlxSprite;
+
+import kiss.Prelude;
+import flash.display.BitmapData;
+import flash.geom.Rectangle;
+import flash.geom.Point;
+import flixel.graphics.FlxGraphic;
+import flixel.util.FlxColor;
+
+@:build(kiss.Kiss.build())
+class SpriteSheet {}
