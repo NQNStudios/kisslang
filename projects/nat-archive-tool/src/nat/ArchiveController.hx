a02ed11898dce1ab46cc542877b9eabad59af929
Nat Quayle Nelson <natquaylenelson@gmail.com>
2022-06-27 18:24:18 +0000

    notes on NAT command arg types

--- projects/nat-archive-tool/src/nat/ArchiveController.hx
+++ projects/nat-archive-tool/src/nat/ArchiveController.hx
@@ -7,13 +7,21 @@ import uuid.Uuid;
 import nat.systems.*;
 
 enum CommandArgType {
+    // When called interactively, automatically pass the currently selected Entry(s) to the function
+    // TODO eventually these will also be passed automatically when called programatically
     SelectedEntry;
     SelectedEntries(min:Null<Int>, max:Null<Int>);
+    // String input
     Text(maxLength:Null<Float>); // max length is a float so Math.POSITIVE_INFINITY can be used
+    // Any number of string inputs:
     VarText(maxLength:Null<Float>);
+    // Numerical input, can be forced to align with a fixed step from the minimum
     Number(min:Null<Float>, max:Null<Float>, inStepsOf:Null<Float>);
+    // Make the user select Entry(s) when called interactively
     OneEntry; // This constructor must be disambiguated from the typedef "Entry"
     Entries(min:Null<Int>, max:Null<Int>);
+    // TODO Tag -- make sure a tag input is a valid haxe variable name for tagsMatch compatibility
+    // TODO VarTag
 }
 
 typedef CommandArg = {
