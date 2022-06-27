7e7053e1322406d1bbd58c828a30c96b9fcdfcdd
Nat Quayle Nelson <natquaylenelson@gmail.com>
2022-06-27 18:22:57 +0000

    pass a UI to NAT entryProcessors

--- projects/nat-archive-tool/src/nat/System.hx
+++ projects/nat-archive-tool/src/nat/System.hx
@@ -3,7 +3,7 @@ package nat;
 import kiss.Prelude;
 
 typedef EntryChecker = (Archive, Entry) -> Bool;
-typedef EntryProcessor = (Archive, Entry) -> Dynamic; // Whatever value is returned will be dropped, but this is easier than requiring ->:Void
+typedef EntryProcessor = (Archive, Entry, ?ArchiveUI) -> Dynamic; // Whatever value is returned will be dropped, but this is easier than requiring ->:Void
 
 @:build(kiss.Kiss.build())
 class System {}
