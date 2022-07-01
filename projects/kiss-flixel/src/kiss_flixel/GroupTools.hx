4b52ccdd9fa9c3cfeb233b0dd143de8699801b07
Nat Quayle Nelson <natquaylenelson@gmail.com>
2022-07-01 23:38:39 +0000

    Forgot to add GroupTools

---
+++ projects/kiss-flixel/src/kiss_flixel/GroupTools.hx
@@ -0,0 +1,12 @@
+package kiss_flixel;
+
+import kiss.Prelude;
+import kiss.List;
+import flixel.FlxObject;
+import flixel.FlxState;
+import flixel.math.FlxRect;
+import flixel.math.FlxPoint;
+import flixel.group.FlxGroup;
+
+@:build(kiss.Kiss.build())
+class GroupTools {}
