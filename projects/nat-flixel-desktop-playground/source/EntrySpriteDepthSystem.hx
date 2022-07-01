10904e93e97e5171e9fdea519aa8fac6781c6b58
Nat Quayle Nelson <natquaylenelson@gmail.com>
2022-07-01 23:37:37 +0000

    forgot to add EntrySpriteDepthSystem

---
+++ projects/nat-flixel-desktop-playground/source/EntrySpriteDepthSystem.hx
@@ -0,0 +1,13 @@
+package;
+
+import kiss.Prelude;
+import kiss.List;
+import nat.Entry;
+import nat.System;
+import nat.BoolExpInterp;
+import nat.ArchiveController;
+import nat.components.Positions;
+import flixel.FlxG;
+
+@:build(kiss.Kiss.build())
+class EntrySpriteDepthSystem extends System {}
