473d9c94c0b11095ae5249555dcde7c8aa76cd24
Nat Quayle Nelson <natquaylenelson@gmail.com>
2022-07-09 01:58:38 +0000

    save jigsaw positions

--- projects/flixel-desktop-habit-puzzle-game/source/HabitState.hx
+++ projects/flixel-desktop-habit-puzzle-game/source/HabitState.hx
@@ -15,6 +15,7 @@ import openfl.geom.Rectangle;
 import openfl.geom.Point;
 import openfl.geom.ColorTransform;
 import flixel.util.FlxSpriteUtil;
+import flixel.util.FlxSave;
 import flixel.input.mouse.FlxMouseEventManager;
 import flixel.addons.display.FlxExtendedSprite;
 import flixel.addons.plugin.FlxMouseControl;
