01df515cc4f8522520121caaf4674ae9a3af0e21
Nat Quayle Nelson <natquaylenelson@gmail.com>
2022-09-08 01:38:58 +0000

    playground systems process connections

--- projects/nat-archive-tool/src/nat/systems/PlaygroundSystem.hx
+++ projects/nat-archive-tool/src/nat/systems/PlaygroundSystem.hx
@@ -6,6 +6,7 @@ import nat.System;
 import nat.components.Position;
 
 typedef PlaygroundEntryProcessor = (Archive, Entry, Position, ?ArchiveUI) -> Dynamic; // Whatever value is returned will be dropped, but this is easier than requiring ->:Void
+typedef PlaygroundConnectionProcessor = (Archive, Entry, Position, Entry, Position, ?ArchiveUI) -> Dynamic; // Whatever value is returned will be dropped, but this is easier than requiring ->:Void
 
 /**
  * Base class for Systems that process Entries in a playground view and displays them in an interactive form
