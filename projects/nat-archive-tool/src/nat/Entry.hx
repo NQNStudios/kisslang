aa05960a673e7fe3eb8c3af1eefa00265dd0dde2
Nat Quayle Nelson <natquaylenelson@gmail.com>
2022-10-08 22:33:41 +0000

    Add toString to nat.Entry

--- projects/nat-archive-tool/src/nat/Entry.hx
+++ projects/nat-archive-tool/src/nat/Entry.hx
@@ -1,7 +1,18 @@
 package nat;
 
-typedef Entry = {
-    id:String,
-    components:Map<String, String>,
-    files:Array<FileRef>
-};
+import uuid.Uuid;
+
+@:jsonParse(function (json) return new nat.Entry(json.id, json.components, json.files))
+class Entry {
+    public var id:String;
+    public var components:Map<String, String> = [];
+    public var files:Array<FileRef> = [];
+    function toString() {
+        return if (components.exists("Name")) components["Name"] else 'entry $id';
+    }
+    public function new(?id:String, ?components:Map<String,String>, ?files:Array<String>) {
+        this.id = if (id != null) id else Uuid.v4();
+        if (components != null) this.components = components;
+        if (files != null) this.files = files;
+    }
+}
