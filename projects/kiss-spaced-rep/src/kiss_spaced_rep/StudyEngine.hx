3d9fe72adae34a32ee88776a421d70a3b07b0ae4
Nat Quayle Nelson <natquaylenelson@gmail.com>
2022-11-03 22:23:50 +0000

    image cards in basic engine

--- projects/kiss-spaced-rep/src/kiss_spaced_rep/StudyEngine.hx
+++ projects/kiss-spaced-rep/src/kiss_spaced_rep/StudyEngine.hx
@@ -6,6 +6,7 @@ typedef StudyEngine = {
     clear: Void -> Void,
     print: (String) -> Void,
     println: (String) -> Void,
+    showImage: (String) -> Void,
     printCC: (String, Continuation) -> Void,
     printlnCC: (String, Continuation) -> Void,
     showImageCC: (String, Continuation) -> Void,
