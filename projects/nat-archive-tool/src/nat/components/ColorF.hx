8579b6b8f0d4bb1f570f26ce0c6bcf594c371969
Nat Quayle Nelson <natquaylenelson@gmail.com>
2022-12-04 23:11:18 +0000

    Use int-based colors for tag-color systems in NAT. Close #167

---
+++ projects/nat-archive-tool/src/nat/components/ColorF.hx
@@ -0,0 +1,8 @@
+package nat.components;
+
+typedef ColorF = {
+    r:Float,
+    g:Float,
+    b:Float,
+    a:Float
+};
---
+++ projects/nat-archive-tool/src/nat/components/ColorI.hx
@@ -0,0 +1,8 @@
+package nat.components;
+
+typedef ColorI = {
+    r:Int,
+    g:Int,
+    b:Int,
+    a:Int
+};
