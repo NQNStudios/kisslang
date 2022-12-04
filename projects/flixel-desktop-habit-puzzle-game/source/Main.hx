c25f112dd18b93a61c4e1b355af40d1814520723
Nat Quayle Nelson <natquaylenelson@gmail.com>
2022-12-04 20:13:49 +0000

    remove initialZoom argument from HaxeFlixel games

--- projects/flixel-desktop-habit-puzzle-game/source/Main.hx
+++ projects/flixel-desktop-habit-puzzle-game/source/Main.hx
@@ -13,7 +13,7 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(0, 0, HabitState, 1, 60, 60, true));
+		addChild(new FlxGame(0, 0, HabitState, 60, 60, true));
 		var t:HabitState = cast FlxG.state;
 		
 		var saveFolder = Prelude.joinPath(Prelude.userHome(), "Documents", "HabitPuzzles");
--- projects/flixel-desktop-handloose/source/Main.hx
+++ projects/flixel-desktop-handloose/source/Main.hx
@@ -9,7 +9,7 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(0, 0, TypingState, 1, 60, 60, true));
+		addChild(new FlxGame(0, 0, TypingState, 60, 60, true));
 		var t:TypingState = cast FlxG.state;
 		t.setModel(new DocumentModel(Sys.args()[0]));
 	}
