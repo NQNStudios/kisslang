97eb91c5b1facdd7d4cf779cbf6daa10f8f79779
Nat Quayle Nelson <natquaylenelson@gmail.com>
2023-01-22 14:37:13 -0700

    kiss-cli-tests test new-flixel-project. Close #174

--- projects/kiss-cli-tests/test.sh
+++ projects/kiss-cli-tests/test.sh
@@ -3,13 +3,26 @@
 # Test kiss new-project command
 if [ -d TestNewProject ]; then
     rm -rf TestNewProject
-fi
-cat new-project-input.txt | haxelib run kiss new-project && (cd TestNewProject && sh test.sh)
+fi &&
+cat new-project-input.txt | haxelib run kiss new-project && (cd TestNewProject && sh test.sh) &&
 
 # Test kiss new-express-project command
 if [ -d testnewexpressproject ]; then
     rm -rf testnewexpressproject
-fi
-cat new-express-project-input.txt | haxelib run kiss new-express-project && (cd testnewexpressproject && sh test.sh)
+fi &&
+cat new-express-project-input.txt | haxelib run kiss new-express-project && (cd testnewexpressproject && sh test.sh) &&
 
+# Test kiss new-flixel-project command
+if [ ! -z "$CI_OS_NAME" ]
+then
+    haxelib install lime
+    haxelib install openfl
+    haxelib install flixel
+fi &&
+if [ -d TestNewFlixelProject ]; then
+    rm -rf TestNewFlixelProject
+fi &&
+cat new-flixel-project-input.txt | haxelib run kiss new-flixel-project && (cd TestNewFlixelProject && haxelib run lime build neko)
+
+# &&
 # TODO test other kiss project templates
