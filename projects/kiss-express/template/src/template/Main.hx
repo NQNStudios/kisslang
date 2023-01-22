c5e7408a1c834d673609a4b2b1ae9e5f903c9275
Nat Quayle Nelson <natquaylenelson@gmail.com>
2023-01-22 14:51:45 -0700

    Remove Kiss.build() from kiss-express template (#175)

--- projects/kiss-express/template/src/template/Main.hx
+++ projects/kiss-express/template/src/template/Main.hx
@@ -1,7 +1,7 @@
 package template;
 
-import kiss.Kiss;
-import kiss.Prelude;
-
-@:build(kiss.Kiss.build())
-class Main {}
+class Main {
+    static function main() {
+        Main_.main();
+    }
+}
