87569c269c19f3e381adbeba38ec9f55562daf91
Nat Quayle Nelson <natquaylenelson@gmail.com>
2023-03-06 16:04:58 -0700

    Hollywoo-flixel local test project

---
+++ projects/kiss-flixel/src/test/source/Main.hx
@@ -0,0 +1,7 @@
+package;
+
+import flixel.FlxGame;
+import openfl.display.Sprite;
+
+@:build(kiss.Kiss.build())
+class Main extends Sprite {}
---
+++ projects/kiss-flixel/src/test/source/TestState.hx
@@ -0,0 +1,41 @@
+package;
+
+import kiss.Prelude;
+import kiss.List;
+import hollywoo_flixel.FlxDirector;
+import hollywoo_flixel.FlxMovie;
+import hollywoo_flixel.MovieFlxState;
+import hollywoo_flixel.ActorFlxSprite;
+import kiss_flixel.SpriteTools;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxSubState;
+import flixel.util.FlxColor;
+import flixel.tweens.FlxTween;
+
+@:build(kiss.AsyncEmbeddedScript.build("hollywoo-flixel", "src/hollywoo_flixel/HollywooFlixelDSL.kiss", "Test.hollywoo"))
+class Test extends FlxMovie {
+    public function new(director:FlxDirector) {
+        super(director);
+    }
+}
+
+class TestState extends MovieFlxState {
+    var t:Test;
+
+    public override function create() {
+        super.create();
+        t = new Test(director);
+        t.run();
+    }
+
+    public override function destroy() {
+        t.doCleanup();
+        super.destroy();
+    }
+
+    public override function update(elapsed:Float) {
+        super.update(elapsed);
+        t.update(elapsed);
+    }
+}
