782f1d1f94e4cc2ae5083c8b9376bca51c08d49d
Nat Quayle Nelson <natquaylenelson@gmail.com>
2023-03-10 13:26:50 -0700

    Finish Kiss-VScode overhaul first pass

--- projects/kiss-vscode/test.sh
+++ projects/kiss-vscode/test.sh
@@ -1,4 +1,3 @@
 #! /bin/bash
 
-# Run the build without -D test first, to make sure it works that way too:
-echo "!test" && haxe build.hxml && echo "test" && haxe -D test build.hxml -cmd "node bin/extension.js"
+haxe build.hxml
