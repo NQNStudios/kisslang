7403212ecbe9e1594176c20cc03d43ce045acec1
Nat Quayle Nelson <natquaylenelson@gmail.com>
2023-03-19 12:11:10 -0600

    Overhaul ktxt2. Close #184

---
+++ projects/ktxt2/README.md
@@ -0,0 +1,11 @@
+# ktxt2 README
+
+A tool for side-by-side, semi-automatic conversion between plaintext formats.
+
+## Features
+
+## Extension Settings
+
+## Known Issues
+
+## Release Notes
---
+++ projects/ktxt2/src/ktxt2/Main.hx
@@ -0,0 +1,14 @@
+package ktxt2;
+
+import kiss.Prelude;
+import kiss.List;
+
+import vscode.*;
+import ktxt2.KTxt2;
+
+import sys.io.File;
+
+using StringTools;
+
+@:build(kiss.Kiss.build())
+class Main {}
