76c8e2283d42ecb44b667e346238ff2a2f81df09
Nat Quayle Nelson <natquaylenelson@gmail.com>
2023-03-29 06:47:45 -0600

    FlxLightSource

---
+++ projects/hollywoo-flixel/src/hollywoo_flixel/FlxLightSource.hx
@@ -0,0 +1,9 @@
+package hollywoo_flixel;
+
+import kiss.Prelude;
+import kiss.List;
+import flixel.math.FlxPoint;
+import flixel.util.FlxColor;
+
+@:build(kiss.Kiss.build())
+class FlxLightSource {}
