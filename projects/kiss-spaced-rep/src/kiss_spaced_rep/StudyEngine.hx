1d140d38aefb9f51367fcf73dc70d842eba3a796
Nat Quayle Nelson <natquaylenelson@gmail.com>
2023-04-12 09:54:09 -0600

    kiss-spaced-rep refresh flow

--- projects/kiss-spaced-rep/src/kiss_spaced_rep/StudyEngine.hx
+++ projects/kiss-spaced-rep/src/kiss_spaced_rep/StudyEngine.hx
@@ -7,6 +7,7 @@ interface StudyEngine {
     function print(s:String):Void;
     function println(s:String):Void;
     function showImage(s:String):Void;
-    function delayForUserInput(cc:Continuation):Void;
+    function delayForUserInput(cc:Continuation, ?text:String):Void;
     function getUserInput(resolve:String->Void):Void;
+    function promptForRefresh(refresh:Void->Void):Void;
 }
