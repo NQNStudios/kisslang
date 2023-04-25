289e516fe3d91b935e74c29e698d0d06815efd0d
Nat Quayle Nelson <natquaylenelson@gmail.com>
2023-04-25 11:38:12 -0600

    Squashed 'projects/tink_syntaxhub/' content from commit 99dbb477
    git-subtree-dir: projects/tink_syntaxhub
    git-subtree-split: 99dbb4774745300a01367e9f371f917208163477

---
+++ tests/Main.hx
@@ -0,0 +1,19 @@
+package;
+import haxe.unit.TestCase;
+import haxe.unit.TestRunner;
+
+class Main extends TestCase {
+  
+  function testTxt()
+    assertEquals('Hello, hello!!!', HelloWorld.TEXT);
+  
+  function testXml()
+    assertEquals('<3', HelloWorld.XML.firstElement().firstChild().nodeValue);
+    
+  static function main() {
+    var t = new TestRunner();
+    t.add(new Main());
+    t.run();
+  }
+  
+}
