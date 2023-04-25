7d4fd6a5e56f4ebf0bffbb0b724b9be163eb2e64
Nat Quayle Nelson <natquaylenelson@gmail.com>
2023-04-25 15:58:12 -0600

    Fix aoc Main_

--- projects/aoc/src/Main.hx
+++ projects/aoc/src/Main.hx
@@ -1,20 +1,7 @@
 package;
 
-import kiss.Kiss;
-import kiss.Prelude;
-import kiss.List;
-#if year2018
-import year2018.Solutions2018;
-#end
-#if year2020
-import year2020.Solutions2020;
-#end
-#if year2021
-import year2021.Solutions2021;
-#end
-#if year2022
-import year2022.Solutions2022;
-#end
-
-@:build(kiss.Kiss.build())
-class Main {}
+class Main {
+    static function main() {
+        Main_.main();
+    }
+}
